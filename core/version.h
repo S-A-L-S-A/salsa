a543f3f84b94b8622d24e63e70c7683ed2092a3c
Tomassino Ferrauto <t_ferrauto@yahoo.it>
2016-08-03 08:52:31 +0200

    Added an empty version.h file

---
+++ core/version.h
@@ -0,0 +1,23 @@
+/***************************************************************************
+ *  SALSA Configuration Library                                            *
+ *  Copyright (C) 2007-2013                                                *
+ *  Gianluca Massera <emmegian@yahoo.it>                                   *
+ *  Tomassino Ferrauto <tomassino.ferrauto@istc.cnr.it>                    *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the                          *
+ *  Free Software Foundation, Inc.,                                        *
+ *  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
+ ***************************************************************************/
+
+QUI METTERE LA VERSIONE DI SALSA E INCLUDERLO DA TUTTI I I *config.h DELLE VARIE LIBRERIE
