28dc0ad4674c1a4ad2fff9ecf2af3bc737d49697
Tomassino Ferrauto <t_ferrauto@yahoo.it>
2016-08-10 00:32:05 +0200

    Automatically generate the header file with version from version specified in the CMakeLists.txt file

--- core/Helpers.cmake
+++ core/Helpers.cmake
@@ -3,3 +3,13 @@
 function(add_salsa_version targetName)
 	set_target_properties(${targetName} PROPERTIES VERSION ${SALSA_VER_MAJ}.${SALSA_VER_MIN}.${SALSA_VER_PAT} SOVERSION ${SALSA_VER_MAJ}.${SALSA_VER_MIN})
 endfunction()
+
+# Simply adds a 0 in front of a number if it is smaller than 0 and returns it
+# as a string (e.g. if num is 7, outStr will be "07"
+function(pad_number_smaller_than_10 outStr num)
+	if(${num} LESS 10)
+		set(${outStr} "0${num}" PARENT_SCOPE)
+	else()
+		set(${outStr} "${num}" PARENT_SCOPE)
+	endif()
+endfunction()
